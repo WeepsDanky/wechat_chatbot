51f060b0ce305abc7fcf2da9fe38357ec2b92f50
Mnk_Saure <50621638+WeepsDanky@users.noreply.github.com>
2023-09-10 22:18:28 +0100

    Test

---
+++ main_page.py
@@ -0,0 +1,12 @@
+"""
+# My first app
+Here"s our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np 
+
+st.markdown("# Main Page! ")
+
+st.sidebar.markdown("# Main Page? ")
---
+++ pages/page_1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Page 1 ❄️")
+st.sidebar.markdown("# Page 1 ❄️")
